a97dc4279e26fda368a66a50355e42969ed612a5
Ferdinand Malcher <ferdinand@malcher.media>
2024-12-09 15:24:45 +0100

    AppComponent leeren

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,4 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'book-monkey6';
-}
+export class AppComponent {}
