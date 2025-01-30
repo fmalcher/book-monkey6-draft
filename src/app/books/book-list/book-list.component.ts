432c8797fe0e96f5569654f79f1034ef6730cded
Danny Koppenhagen <Danny.Koppenhagen@deutschebahn.com>
2025-01-30 11:35:07 +0100

    feat: add styles

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -1,14 +1,12 @@
-import { Component, effect, signal, inject, computed, input, linkedSignal } from '@angular/core';
+import { Component, effect, signal, inject, input, linkedSignal } from '@angular/core';
 import { Book } from '../../shared/book';
 import { BookListItemComponent } from '../book-list-item/book-list-item.component';
 import { BookStoreService } from '../../shared/book-store.service';
-import { rxResource } from '@angular/core/rxjs-interop';
 import { Router } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-book-list',
-  imports: [BookListItemComponent, FormsModule],
+  imports: [BookListItemComponent],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.scss'
 })
